be652917be3d4713767f76a983ffe253fd5ab9d1
GovorukhinVadim <v081297@mail.ru>
2022-06-18 11:00:15 +0300

    HW2 is dirty completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,56 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	result := ""
+	runes := []rune(s)
+	r_tmp := "error"
+	var flag bool
+	r_s := ""
+
+	for i, r := range runes {
+		if flag {
+			flag = false
+			continue
+		}
+
+		if r == rune('\\') {
+			r_s = string(r) + string(runes[i+1])
+			flag = true
+		} else {
+			r_s = string(r)
+		}
+
+		switch {
+		case unicode.IsDigit(r):
+			num, _ := strconv.Atoi(r_s)
+			if r_tmp == "error" {
+				return "", ErrInvalidString
+			}
+			if num == 0 {
+				result = strings.TrimSuffix(result, r_tmp)
+			} else {
+				for j := 1; j < num; j++ {
+					result += r_tmp
+				}
+			}
+			r_tmp = "error"
+
+		case unicode.IsLetter(r) || unicode.IsSpace(r):
+			r_tmp = r_s
+			result += r_tmp
+		default:
+			return "", ErrInvalidString
+
+		}
+	}
+
+	return result, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -9,13 +9,17 @@ import (
 
 func TestUnpack(t *testing.T) {
 	tests := []struct {
+		name     string
 		input    string
 		expected string
 	}{
-		{input: "a4bc2d5e", expected: "aaaabccddddde"},
-		{input: "abccd", expected: "abccd"},
-		{input: "", expected: ""},
-		{input: "aaa0b", expected: "aab"},
+		{name: "empty", input: "", expected: ""},
+		{name: "charnum", input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{name: "two equal chars", input: "abccd", expected: "abccd"},
+		{name: "char0", input: "aaa0b", expected: "aab"},
+		{name: "char1", input: "a1b", expected: "ab"},
+		{name: "rus", input: "б2ж3й1", expected: "ббжжжй"},
+		{name: "shieldchar", input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -25,7 +29,7 @@ func TestUnpack(t *testing.T) {
 
 	for _, tc := range tests {
 		tc := tc
-		t.Run(tc.input, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, result)
