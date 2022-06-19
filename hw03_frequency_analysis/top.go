e3d2cdbed829921b78747ae05320d83fb630db6a
GovorukhinVadim <v081297@mail.ru>
2022-06-19 09:52:12 +0300

    HW3 completed, before linters

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,63 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
+
+type FrequencyAnalizer struct {
+	freq_map map[string]int
+	top      []string
+}
+
+func (f FrequencyAnalizer) Len() int {
+	return len(f.top)
+}
+
+func (f FrequencyAnalizer) Swap(i, j int) {
+	f.top[i], f.top[j] = f.top[j], f.top[i]
+}
+
+func (f FrequencyAnalizer) Less(i, j int) bool {
+	if f.freq_map[f.top[i]] == f.freq_map[f.top[j]] {
+		return f.top[i] < f.top[j]
+	}
+	return f.freq_map[f.top[i]] > f.freq_map[f.top[j]]
+}
+
+func Top10(str string) []string {
+	freqAn := FrequencyAnalizer{
+		freq_map: make(map[string]int),
+	}
+	splitted := strings.FieldsFunc(strings.ToLower(str), separatorRule)
+	for _, word := range splitted {
+		if word == "-" {
+			continue
+		}
+		val, ok := freqAn.freq_map[word]
+		if ok {
+			val++
+		} else {
+			val = 1
+		}
+		freqAn.freq_map[word] = val
+	}
+
+	//fmt.Println(f.freq_map)
+
+	for key := range freqAn.freq_map {
+		freqAn.top = append(freqAn.top, key)
+	}
+	sort.Sort(freqAn)
+
+	resultLen := 10
+	if len(freqAn.top) < resultLen {
+		resultLen = len(freqAn.top)
+	}
+	return freqAn.top[:resultLen]
+}
+
+func separatorRule(c rune) bool {
+	return unicode.IsSpace(c) || (unicode.IsPunct(c) && c != rune('-'))
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -6,9 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// Change to true if needed.
-var taskWithAsteriskIsCompleted = false
-
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
 	ступеньки собственным затылком:  бум-бум-бум.  Другого  способа
@@ -44,13 +41,32 @@ var text = `Как видите, он  спускается  по  лестни�
 		В этот вечер...`
 
 func TestTop10(t *testing.T) {
-	t.Run("no words in empty string", func(t *testing.T) {
-		require.Len(t, Top10(""), 0)
-	})
-
-	t.Run("positive test", func(t *testing.T) {
-		if taskWithAsteriskIsCompleted {
-			expected := []string{
+	tests := []struct {
+		name     string
+		input    string
+		expected []string
+	}{
+		{name: "empty", input: "", expected: []string(nil)},
+		{name: "capitallet", input: "Cat and dog, cat And Dog, cat and dog donkey!",
+			expected: []string{
+				"and",    // 3
+				"cat",    // 3
+				"dog",    // 3
+				"donkey", // 1
+			}},
+		{name: "punct", input: "cat and dog, one dog,two cats and one man very-very",
+			expected: []string{
+				"and",       // 2
+				"dog",       // 2
+				"one",       // 2
+				"cat",       // 1
+				"cats",      // 1
+				"man",       // 1
+				"two",       // 1
+				"very-very", // 1
+			}},
+		{name: "textWinnierus", input: text,
+			expected: []string{
 				"а",         // 8
 				"он",        // 8
 				"и",         // 6
@@ -61,22 +77,13 @@ func TestTop10(t *testing.T) {
 				"если",      // 4
 				"кристофер", // 4
 				"не",        // 4
-			}
-			require.Equal(t, expected, Top10(text))
-		} else {
-			expected := []string{
-				"он",        // 8
-				"а",         // 6
-				"и",         // 6
-				"ты",        // 5
-				"что",       // 5
-				"-",         // 4
-				"Кристофер", // 4
-				"если",      // 4
-				"не",        // 4
-				"то",        // 4
-			}
-			require.Equal(t, expected, Top10(text))
-		}
-	})
+			}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Top10(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
 }
