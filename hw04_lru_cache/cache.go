6a991339a7dd3511f9eb43cc1dd1a2bcb8b4b389
GovorukhinVadim <v081297@mail.ru>
2022-06-21 17:28:59 +0300

    HW4_Cache is testingd

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -3,17 +3,15 @@ package hw04lrucache
 type Key string
 
 type Cache interface {
-	Set(key Key, value interface{}) bool
-	Get(key Key) (interface{}, bool)
-	Clear()
+	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу
+	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу
+	Clear()                              // Очистить кэш.
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
-	capacity int
-	queue    List
-	items    map[Key]*ListItem
+	capacity int               // Количество сохраняемых в кэше элементов
+	queue    List              // очередь [последних используемых элементов] на основе двусвязного списка
+	items    map[Key]*ListItem // словарь, отображающий ключ (строка) на элемент очереди
 }
 
 type cacheItem struct {
@@ -27,4 +25,38 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	_, ok := c.items[key]
+	if ok {
+		item := c.queue.PushFront(value)
+		c.items[key] = item
+	} else {
+		item := c.queue.PushFront(value)
+		c.items[key] = item
+		if c.queue.Len() > c.capacity {
+			deleteitem := c.queue.Back()
+			c.queue.Remove(deleteitem)
+			for deletekey, val := range c.items {
+				if val.Value == deleteitem.Value {
+					delete(c.items, deletekey)
+				}
+			}
+		}
+	}
+	return ok
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	item, ok := c.items[key]
+	if ok {
+		c.queue.MoveToFront(item)
+		return item.Value, ok
+	}
+	return nil, ok
+}
+
+func (c *lruCache) Clear() {
+	// TO DO
 }
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -10,44 +10,112 @@ import (
 )
 
 func TestCache(t *testing.T) {
-	t.Run("empty cache", func(t *testing.T) {
-		c := NewCache(10)
+	testcases := []struct {
+		name    string
+		runfunc func(t *testing.T)
+	}{
+		{
+			name: "empty cache",
+			runfunc: func(t *testing.T) {
+				c := NewCache(10)
 
-		_, ok := c.Get("aaa")
-		require.False(t, ok)
+				_, ok := c.Get("aaa")
+				require.False(t, ok)
 
-		_, ok = c.Get("bbb")
-		require.False(t, ok)
-	})
+				_, ok = c.Get("bbb")
+				require.False(t, ok)
+			},
+		},
+		{
+			name: "add to cache",
+			runfunc: func(t *testing.T) {
+				c := NewCache(2)
 
-	t.Run("simple", func(t *testing.T) {
-		c := NewCache(5)
+				_, ok := c.Get("aaa")
+				require.False(t, ok)
 
-		wasInCache := c.Set("aaa", 100)
-		require.False(t, wasInCache)
+				wasInCache := c.Set("aaa", 100)
+				require.False(t, wasInCache)
 
-		wasInCache = c.Set("bbb", 200)
-		require.False(t, wasInCache)
+				val, ok := c.Get("aaa")
+				require.True(t, ok)
+				require.Equal(t, 100, val)
+			},
+		},
+		{
+			name: "rewrite item",
+			runfunc: func(t *testing.T) {
+				c := NewCache(2)
 
-		val, ok := c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 100, val)
+				wasInCache := c.Set("aaa", 100)
+				require.False(t, wasInCache)
 
-		val, ok = c.Get("bbb")
-		require.True(t, ok)
-		require.Equal(t, 200, val)
+				wasInCache = c.Set("aaa", 1)
+				require.True(t, wasInCache)
 
-		wasInCache = c.Set("aaa", 300)
-		require.True(t, wasInCache)
+				val, ok := c.Get("aaa")
+				require.True(t, ok)
+				require.Equal(t, 1, val)
+			},
+		},
+		{
+			name: "cacheoverflow",
+			runfunc: func(t *testing.T) {
+				c := NewCache(2)
 
-		val, ok = c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 300, val)
+				c.Set("aaa", 1)
+				c.Set("bbb", 2)
+				c.Set("ccc", 3)
 
-		val, ok = c.Get("ccc")
-		require.False(t, ok)
-		require.Nil(t, val)
-	})
+				val, ok := c.Get("aaa")
+				require.False(t, ok)
+				require.Nil(t, val)
+
+				val, ok = c.Get("bbb")
+				require.True(t, ok)
+				require.Equal(t, 2, val)
+
+				val, ok = c.Get("ccc")
+				require.True(t, ok)
+				require.Equal(t, 3, val)
+			},
+		},
+		{
+			name: "simple",
+			runfunc: func(t *testing.T) {
+				c := NewCache(5)
+
+				wasInCache := c.Set("aaa", 100)
+				require.False(t, wasInCache)
+
+				wasInCache = c.Set("bbb", 200)
+				require.False(t, wasInCache)
+
+				val, ok := c.Get("aaa")
+				require.True(t, ok)
+				require.Equal(t, 100, val)
+
+				val, ok = c.Get("bbb")
+				require.True(t, ok)
+				require.Equal(t, 200, val)
+
+				wasInCache = c.Set("aaa", 300)
+				require.True(t, wasInCache)
+
+				val, ok = c.Get("aaa")
+				require.True(t, ok)
+				require.Equal(t, 300, val)
+
+				val, ok = c.Get("ccc")
+				require.False(t, ok)
+				require.Nil(t, val)
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, tc.runfunc)
+	}
 
 	t.Run("purge logic", func(t *testing.T) {
 		// Write me
