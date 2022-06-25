34cb9a017b08daea45f77bcbff48316571073aad
GovorukhinVadim <v081297@mail.ru>
2022-06-25 11:05:13 +0300

    hw04_cache is completed and lintered

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -10,199 +10,125 @@ import (
 )
 
 func TestCache(t *testing.T) {
-	testcases := []struct {
-		name    string
-		runfunc func(t *testing.T)
-	}{
-		{
-			name: "empty cache",
-			runfunc: func(t *testing.T) {
-				t.Helper()
-				c := NewCache(10)
+	t.Run("empty cache", func(t *testing.T) {
+		c := NewCache(10)
 
-				val, ok := c.Get("aaa")
-				require.False(t, ok)
-				require.Nil(t, val)
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
 
-				val, ok = c.Get("bbb")
-				require.False(t, ok)
-				require.Nil(t, val)
-			},
-		},
-		{
-			name: "add to cache",
-			runfunc: func(t *testing.T) {
-				t.Helper()
-				c := NewCache(2)
+		val, ok = c.Get("bbb")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
 
-				wasInCache := c.Set("aaa", 100)
-				require.False(t, wasInCache)
+	t.Run("add different elemns to cache", func(t *testing.T) {
+		c := NewCache(2)
 
-				val, ok := c.Get("aaa")
-				require.True(t, ok)
-				require.Equal(t, 100, val)
-			},
-		},
-		{
-			name: "rewrite item",
-			runfunc: func(t *testing.T) {
-				t.Helper()
-				c := NewCache(2)
+		wasInCache := c.Set("aaa", 1)
+		require.False(t, wasInCache)
 
-				c.Set("aaa", 100)
+		val, wasInCache := c.Get("aaa")
+		require.True(t, wasInCache)
+		require.Equal(t, 1, val)
 
-				wasInCache := c.Set("aaa", 1)
-				require.True(t, wasInCache)
+		c.Set("это тест", "и это тест")
+		val, wasInCache = c.Get("это тест")
+		require.True(t, wasInCache)
+		require.Equal(t, "и это тест", val)
+	})
 
-				val, wasInCache := c.Get("aaa")
-				require.True(t, wasInCache)
-				require.Equal(t, 1, val)
-			},
-		},
-		{
-			name: "overflow cache",
-			runfunc: func(t *testing.T) {
-				t.Helper()
-				c := NewCache(2)
+	t.Run("rewrite item", func(t *testing.T) {
+		c := NewCache(2)
+		c.Set("aaa", 100)
 
-				c.Set("aaa", 1) // [1]
-				c.Set("bbb", 2) // [2, 1]
-				c.Set("ccc", 3) // [3, 2]
+		wasInCache := c.Set("aaa", 1)
+		require.True(t, wasInCache)
 
-				val, ok := c.Get("aaa")
-				require.False(t, ok)
-				require.Nil(t, val)
+		val, wasInCache := c.Get("aaa")
+		require.True(t, wasInCache)
+		require.Equal(t, 1, val)
+	})
 
-				val, ok = c.Get("bbb")
-				require.True(t, ok)
-				require.Equal(t, 2, val)
+	t.Run("overflow cache", func(t *testing.T) {
+		c := NewCache(2)
 
-				val, ok = c.Get("ccc")
-				require.True(t, ok)
-				require.Equal(t, 3, val)
-			},
-		},
-		{
-			name: "clear cache",
-			runfunc: func(t *testing.T) {
-				t.Helper()
-				c := NewCache(2)
+		c.Set("aaa", 1) // [1]
+		c.Set("bbb", 2) // [2, 1]
+		c.Set("ccc", 3) // [3, 2]
 
-				c.Set("aaa", 1)
-				c.Set("bbb", 2)
-				c.Clear()
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
 
-				val, wasInCache := c.Get("aaa")
-				require.False(t, wasInCache)
-				require.Nil(t, val)
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 2, val)
 
-				val, wasInCache = c.Get("bbb")
-				require.False(t, wasInCache)
-				require.Nil(t, val)
-			},
-		},
-		{
-			name: "different types of elements",
-			runfunc: func(t *testing.T) {
-				t.Helper()
-				c := NewCache(2)
+		val, ok = c.Get("ccc")
+		require.True(t, ok)
+		require.Equal(t, 3, val)
+	})
 
-				c.Set("aaa", 1)
-				c.Set("это тест", "и это тест")
+	t.Run("clear cache", func(t *testing.T) {
+		c := NewCache(2)
 
-				val, wasInCache := c.Get("aaa")
-				require.True(t, wasInCache)
-				require.Equal(t, 1, val)
+		c.Set("aaa", 1)
+		c.Set("bbb", 2)
+		c.Clear()
 
-				val, wasInCache = c.Get("это тест")
-				require.True(t, wasInCache)
-				require.Equal(t, "и это тест", val)
-			},
-		},
-		{
-			name: "simple",
-			runfunc: func(t *testing.T) {
-				t.Helper()
-				c := NewCache(5)
+		val, wasInCache := c.Get("aaa")
+		require.False(t, wasInCache)
+		require.Nil(t, val)
 
-				wasInCache := c.Set("aaa", 100)
-				require.False(t, wasInCache)
+		val, wasInCache = c.Get("bbb")
+		require.False(t, wasInCache)
+		require.Nil(t, val)
+	})
 
-				wasInCache = c.Set("bbb", 200)
-				require.False(t, wasInCache)
+	t.Run("purge logic", func(t *testing.T) {
+		c := NewCache(3)
 
-				val, wasInCache := c.Get("aaa") // [100, 200]
-				require.True(t, wasInCache)
-				require.Equal(t, 100, val)
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+		c.Set("bbb", 400) // [400, 300, 100]
 
-				val, wasInCache = c.Get("bbb") // [200, 100]
-				require.True(t, wasInCache)
-				require.Equal(t, 200, val)
+		val, ok := c.Get("aaa") // [100, 400, 300]
+		require.True(t, ok)
+		require.Equal(t, 100, val)
 
-				wasInCache = c.Set("aaa", 300) // [300, 200]
-				require.True(t, wasInCache)
+		val, ok = c.Get("bbb") // [400, 100, 300]
+		require.True(t, ok)
+		require.Equal(t, 400, val)
 
-				val, wasInCache = c.Get("aaa")
-				require.True(t, wasInCache)
-				require.Equal(t, 300, val)
+		val, ok = c.Get("ccc") // [300, 400, 100]
+		require.True(t, ok)
+		require.Equal(t, 300, val)
 
-				val, wasInCache = c.Get("ccc")
-				require.False(t, wasInCache)
-				require.Nil(t, val)
-			},
-		},
-		{
-			name: "purge logic",
-			runfunc: func(t *testing.T) {
-				t.Helper()
-				c := NewCache(3)
+		val, ok = c.Get("ddd") // [300, 400, 100]
+		require.False(t, ok)
+		require.Nil(t, val)
 
-				c.Set("aaa", 100)
-				c.Set("bbb", 200)
-				c.Set("ccc", 300)
-				c.Set("bbb", 400) // [400, 300, 100]
+		c.Set("ddd", 500)
+		c.Set("aaa", 600)
 
-				val, ok := c.Get("aaa") // [100, 400, 300]
-				require.True(t, ok)
-				require.Equal(t, 100, val)
+		val, ok = c.Get("aaa") // [600, 500, 300]
+		require.True(t, ok)
+		require.Equal(t, 600, val)
 
-				val, ok = c.Get("bbb") // [400, 100, 300]
-				require.True(t, ok)
-				require.Equal(t, 400, val)
+		val, ok = c.Get("bbb")
+		require.False(t, ok)
+		require.Nil(t, val)
 
-				val, ok = c.Get("ccc") // [300, 400, 100]
-				require.True(t, ok)
-				require.Equal(t, 300, val)
+		val, ok = c.Get("ccc")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
 
-				val, ok = c.Get("ddd") // [300, 400, 100]
-				require.False(t, ok)
-				require.Nil(t, val)
-
-				c.Set("ddd", 500)
-				c.Set("aaa", 600)
-
-				val, ok = c.Get("aaa")
-				require.True(t, ok)
-				require.Equal(t, 600, val)
-
-				val, ok = c.Get("bbb")
-				require.False(t, ok)
-				require.Nil(t, val)
-
-				val, ok = c.Get("ccc")
-				require.True(t, ok)
-				require.Equal(t, 300, val)
-
-				val, ok = c.Get("ddd")
-				require.True(t, ok)
-				require.Equal(t, 500, val)
-			},
-		},
-	}
-
-	for _, tc := range testcases {
-		t.Run(tc.name, tc.runfunc)
-	}
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 500, val)
+	})
 }
 
 func TestCacheMultithreading(t *testing.T) {
