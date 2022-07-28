b2c339141c918f1c09d27d29af8201bb53e49bd8
GovorukhinVadim <v081297@mail.ru>
2022-07-28 19:52:16 +0300

    HW10 dummy async solution

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -5,8 +5,16 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
+	"os"
 	"regexp"
 	"strings"
+	"sync"
+)
+
+var (
+	infoLog  = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)                 // for info message
+	errorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) // for error message
 )
 
 type User struct {
@@ -21,15 +29,85 @@ type User struct {
 
 type DomainStat map[string]int
 
-func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
 type users [100_000]User
+
+func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
+	nWorkers := 5
+	var tasks = make(chan string)
+
+	var usersCh = make(chan User, 2*nWorkers)
+
+	var errors = make(chan error)
+	defer close(errors)
+
+	var wg sync.WaitGroup
+	// Workers.
+	wg.Add(nWorkers)
+	for i := 0; i < nWorkers; i++ {
+		go func(i int) {
+			defer wg.Done()
+			defer infoLog.Printf("[goroutine %v] end\n", i)
+			infoLog.Printf("[goroutine %v] start\n", i)
+			var err error
+			for task := range tasks {
+				infoLog.Printf("[goroutine %v] take task '%s'\n", i, task)
+				var user User
+				if err = json.Unmarshal([]byte(task), &user); err != nil {
+					errorLog.Printf("[goroutine %v] exit with error: %s", i, err)
+					errors <- err
+					break
+				}
+				usersCh <- user
+				infoLog.Printf("[goroutine %v] send user", i)
+			}
+
+		}(i)
+	}
+
+	go func() {
+		content, err := ioutil.ReadAll(r)
+		if err != nil {
+			errors <- err
+			return
+		}
+		infoLog.Println("[sender] start sending tasks")
+		lines := strings.Split(string(content), "\n")
+		for _, line := range lines {
+			infoLog.Println("[sender] sending task")
+			tasks <- line
+		}
+		infoLog.Println("[sender] send all tasks, close tasks")
+		close(tasks)
+		wg.Wait()
+		infoLog.Println("[sender] all goroutines stopped, close usersCh")
+		close(usersCh)
+	}()
+
+	var u users
+	var i int
+	for {
+		select {
+		case err := <-errors:
+			errorLog.Printf("[main] get error %s", err)
+			close(tasks)
+			return nil, fmt.Errorf("get users error: %w", err)
+		case user, ok := <-usersCh:
+			if !ok {
+				infoLog.Print("[main] received all users")
+				return countDomains(u, domain)
+			}
+			u[i] = user
+			i++
+		}
+	}
+	/*
+			u, err := getUsers(r)
+		if err != nil {
+			return nil, fmt.Errorf("get users error: %w", err)
+		}
+		return countDomains(u, domain)
+	*/
+}
 
 func getUsers(r io.Reader) (result users, err error) {
 	content, err := ioutil.ReadAll(r)
