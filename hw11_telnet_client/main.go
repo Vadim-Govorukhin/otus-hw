1de53105bb0a47778d051feb78c3dd1f4c350400
GovorukhinVadim <v081297@mail.ru>
2022-08-29 13:46:37 +0300

    first steps

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,37 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
 func main() {
+	var timeout string
+
+	flag.StringVar(&timeout, "timeout", "10s", "timeout of connection")
+	flag.Parse()
+
+	dur, err := time.ParseDuration(timeout)
+	if err != nil {
+		log.Fatalf("Cannot parse timeout: %v", err)
+	}
+
+	arguments := os.Args
+	fmt.Println(arguments)
+	if len(arguments) != 3 {
+		log.Printf("Usage: %s host port ", os.Args[0])
+		os.Exit(1)
+	}
+
+	HOST := arguments[1]
+	PORT := arguments[2]
+	address := HOST + ":" + PORT
+	t := NewTelnetClient(address, dur, os.Stdin, os.Stdout)
+	t.Connect()
+
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,9 +13,36 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type Telnet struct {
+	conn    net.Conn
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	return Telnet{nil, address, timeout, in, out}
+}
+
+func (t Telnet) Connect() (err error) {
+	t.conn, err = net.DialTimeout("tcp", t.address, t.timeout)
+	return err
+}
+
+func (t Telnet) Close() (err error) {
+	err = t.conn.Close()
+	return
+}
+
+func (t Telnet) Receive() (err error) {
+	//_, err = t.in.Read("\n")
+	return
+}
+
+func (t Telnet) Send() (err error) {
+	_, err = t.out.Write([]byte("Received"))
+	return
 }
 
 // Place your code here.
