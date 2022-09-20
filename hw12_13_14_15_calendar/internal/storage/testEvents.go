ca281c7246098eea4733e59e67df47156d610ad8
GovorukhinVadim <v081297@mail.ru>
2022-09-20 14:16:01 +0300

    HW12 storage/sql add test of listings methods#2

---
+++ hw12_13_14_15_calendar/internal/storage/testEvents.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"time"
+
+	"github.com/Vadim-Govorukhin/otus-hw/hw12_13_14_15_calendar/internal/model"
+	"github.com/google/uuid"
+)
+
+var (
+	TestEvent = model.Event{
+		ID:             uuid.New(),
+		Title:          "Test0",
+		StartDate:      time.Date(2022, time.September, 15, 1, 2, 3, 0, time.Local),
+		EndDate:        time.Date(2022, time.December, 15, 1, 2, 3, 0, time.Local),
+		Description:    "A Testing Event0",
+		UserID:         0,
+		NotifyUserTime: time.Duration(24 * time.Hour).Seconds(),
+	}
+	TestEvent2 = model.Event{
+		ID:             uuid.New(),
+		Title:          "Test1",
+		StartDate:      time.Date(2022, time.September, 16, 1, 2, 3, 0, time.Local),
+		EndDate:        time.Date(2022, time.December, 15, 1, 2, 3, 0, time.Local),
+		Description:    "A Testing Event1",
+		UserID:         1,
+		NotifyUserTime: time.Duration(24 * time.Hour).Seconds(),
+	}
+	TestEvent3 = model.Event{
+		ID:             uuid.New(),
+		Title:          "Test2",
+		StartDate:      time.Date(2022, time.November, 16, 1, 2, 3, 0, time.Local),
+		EndDate:        time.Date(2022, time.December, 15, 1, 2, 3, 0, time.Local),
+		Description:    "A Testing Event2",
+		UserID:         0,
+		NotifyUserTime: time.Duration(24 * time.Hour).Seconds(),
+	}
+)
